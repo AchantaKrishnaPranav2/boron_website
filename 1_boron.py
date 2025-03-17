caa30826cc791e261572b3f2a29af98acc42a173
AchantaKrishnaPranav2 <ch23btech11005@iith.ac.in>
2025-03-17 23:26:47 +0530

    Create 1_boron.py

---
+++ 1_boron.py
@@ -0,0 +1,9 @@
+boron-website/
+│── 1_boron.py  # Main Home Page (Remains in the root directory)
+│── pages/  # This folder contains additional pages
+│   ├── Boron_Isotopes.py
+│   ├── Boron_Atomic_Properties.py
+│   ├── Boron_Structure.py
+│   ├── Boron_Occurrences_and_Applications.py
+│── requirements.txt  # Lists necessary Python libraries
+│── README.md  # Description of the project
