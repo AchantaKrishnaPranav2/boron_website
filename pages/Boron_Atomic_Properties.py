c161c3c7be038697ebae15bb221c6cc283e5da11
AchantaKrishnaPranav2 <ch23btech11005@iith.ac.in>
2025-03-18 22:40:04 +0530

    Update Boron_Atomic_Properties.py

--- pages/Boron_Atomic_Properties.py
+++ pages/Boron_Atomic_Properties.py
@@ -147,9 +147,6 @@ df_table = pd.DataFrame({
 
 st.dataframe(df_table, use_container_width=True)
 
-# ✅ **4️⃣ Original Image from NIST**
-st.image("https://www.nist.gov/sites/default/files/styles/960_x_960_limit/public/images/2020/04/20/boron_spectrum.png", 
-         caption="Emission Spectrum of Boron", use_container_width=True)
 
 st.divider()
 
