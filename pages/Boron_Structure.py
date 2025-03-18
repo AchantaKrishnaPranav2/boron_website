3535a5b1e5ff5e947eb716abcc2bfca8c4c87e25
AchantaKrishnaPranav2 <ch23btech11005@iith.ac.in>
2025-03-19 00:52:13 +0530

    Update Boron_Structure.py

--- pages/Boron_Structure.py
+++ pages/Boron_Structure.py
@@ -2,7 +2,7 @@ import streamlit as st
 import plotly.graph_objects as go
 
 st.set_page_config(
-    page_title="Boron - Structure",
+    page_title="Structure",
     page_icon="🧩",
 )
 a = st.radio(
